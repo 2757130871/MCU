51af1cd082f360fae889992ce2a74ba471e86bba
2757130871 <76026098+2757130871@users.noreply.github.com>
2021-06-25 23:45:31 +0800

    Add files via upload

---
+++ RFID设计/BSP/WDG/wdg.c
@@ -0,0 +1,36 @@
+#include "wdg.h"
+
+void IWDG_Init(u8 prer,u16 rlr) 
+{
+	IWDG->KR=0X5555;//使能对IWDG->PR和IWDG->RLR的写		 										  
+  	IWDG->PR=prer;  //设置分频系数   
+  	IWDG->RLR=rlr;  //从加载寄存器 IWDG->RLR  
+	IWDG->KR=0XAAAA;//reload											   
+  	IWDG->KR=0XCCCC;//使能看门狗	
+}
+//喂独立看门狗
+void IWDG_Feed(void)
+{
+	IWDG->KR=0XAAAA;//reload											   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
